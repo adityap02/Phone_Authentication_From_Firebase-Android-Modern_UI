babf078cecd297329d95d8debadf23f31991ca5b
aditya <aditya.porwal22@gmail.com>
2020-06-03 00:56:09 +0530

    phone verification 0.1

--- app/src/main/java/com/example/app2/LoginActivity.java
+++ app/src/main/java/com/example/app2/LoginActivity.java
@@ -4,89 +4,114 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+import io.opencensus.tags.Tag;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText mEmail,mPassword;
-    Button mLoginBtn;
-    TextView mCreateBtn;
+    public static final String TAG = "TAG";
+    EditText phone,code;
+    Button LoginBtn;
+    TextView resend,state;
     ProgressBar progressbar;
     FirebaseAuth fAuth;
+    String verificationID;
+    PhoneAuthProvider.ForceResendingToken token;
+    //Layout one;
+  //  LinearLayout one;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
 
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
-        setContentView(R.layout.activity_main);
+       fAuth = FirebaseAuth.getInstance();
+       phone= findViewById(R.id.phone);
+       code = findViewById(R.id.codeEnter);
+       LoginBtn= findViewById(R.id.cirLoginButton);
+       resend = findViewById(R.id.resend);
+       progressbar= findViewById(R.id.progressbar);
+       state=findViewById(R.id.state);
+   //    one.findViewById(R.id.linearLayout1);
 
-        mEmail = findViewById(R.id.editTextEmail);
-        mPassword = findViewById(R.id.editTextPassword);
-        progressbar = findViewById(R.id.progressBar);
-        fAuth = FirebaseAuth.getInstance();
-        mLoginBtn=findViewById(R.id.cirLoginButton);
 
-        if(fAuth.getCurrentUser() != null){
-            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            finish();
-        }
+       LoginBtn.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+               if(!phone.getText().toString().isEmpty()&& phone.getText().toString().length() == 10){
+                String phNum = "+91"+phone.getText().toString();
+                Log.d(TAG,"Phone Number is " + phNum);
+            //    one.setVisibility(View.VISIBLE);
 
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = mEmail.getText().toString().trim();
-                String password = mPassword.getText().toString().trim();
-                if(TextUtils.isEmpty(email)){
-                    mEmail.setError("Email is Empty");
-                    return;
-                }
-                if(TextUtils.isEmpty(password)){
-                    mPassword.setError("Password is Required");
-                    return;
-                }
+                requestOtp(phNum);
 
-                if(password.length()<6){
-                    mPassword.setError("Password Must be Greater Than 6 Characters");
-                }
-                progressbar.setVisibility(View.VISIBLE);
 
-                // Authenticate
 
-                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this,"Logged In",Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        }
+               }
+               else{
+                   phone.setError("Phone Number Not Valid");
+               }
 
-                        else{
-                            Toast.makeText(LoginActivity.this,"Error"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-                            progressbar.setVisibility(View.GONE);
-                        }
-                    }
-                });
+           }
+       });
 
-            }
-        });
 
     }
+
+    private void requestOtp(String phNum) {
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(phNum, 60L, TimeUnit.SECONDS, this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                verificationID=s;
+                token=forceResendingToken;
+
+
+            }
+
+            @Override
+            public void onCodeAutoRetrievalTimeOut(String s) {
+                super.onCodeAutoRetrievalTimeOut(s);
+            }
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(LoginActivity.this,"Cannot Send OTP"+e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
+/*
+
     public void onLoginClick(View View){
         startActivity(new Intent(this,RegisterActivity.class));
         overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
 
-    }
-}
+    }*/
